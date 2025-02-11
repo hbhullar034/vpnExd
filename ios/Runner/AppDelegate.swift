5a09a52c11d04a84571d4af461073331dc7a8634
SHIVAM SRIVASTAVA <ssrivastava@cogniter.com>
2025-02-11 09:44:36 +0530

    vpn setting add

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,5 @@
-import Flutter
 import UIKit
+import Flutter
 
 @main
 @objc class AppDelegate: FlutterAppDelegate {
@@ -7,7 +7,26 @@ import UIKit
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    
+    let controller: FlutterViewController = window?.rootViewController as! FlutterViewController
+    let permissionsChannel = FlutterMethodChannel(name: "com.example.vpn/permissions",
+                                                  binaryMessenger: controller.binaryMessenger)
+    
+    permissionsChannel.setMethodCallHandler { (call: FlutterMethodCall, result: @escaping FlutterResult) in
+      if call.method == "requestPermission" {
+        self.requestPermission(result: result)
+      } else {
+        result(FlutterMethodNotImplemented)
+      }
+    }
+    
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+  }
+
+  private func requestPermission(result: @escaping FlutterResult) {
+    // Add your permission request logic here.
+    // Example: Always return true for testing purposes
+    result(true)
   }
 }
